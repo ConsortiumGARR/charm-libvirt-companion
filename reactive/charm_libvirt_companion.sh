ea0c6ada0e2ab3709844d4b0538be5594fb78bac
Claudio Pisa <claudio.pisa@garr.it>
2021-09-23 09:21:11 +0000

    first draft

--- reactive/charm_libvirt_companion.sh
+++ reactive/charm_libvirt_companion.sh
@@ -20,5 +20,28 @@ function install_charm-libvirt-companion() {
     charms.reactive set_state 'charm-libvirt-companion.installed'
 }
 
+do_blkdeviotuning() {
+    # tune the block devices of all domains except the ones to be excluded, specified in the configuration option
+    ALLDOMAINS="$( virsh list | tail -n+3 | awk '{print $2}' | grep -v '^$' )"
+    UUIDS="$( echo "$ALLDOMAINS" | | while read instance; do echo -n $instance " "; virsh dumpxml $instance | grep '<uuid>' | sed 's_<uuid>\(.*\)</uuid>_\1_g' | tr -d ' '; done )"
+    EXCLUDED_DOMAINS_GREP="$( config-get excluded_instances | sed -e 's/^/grep -v -e /g' -e 's/\ / -e /g')"
+    DOMAINS="$( echo "$UUIDS" | $EXCLUDED_DOMAINS_GREP | awk '{print $1}' )"
+    echo "$DOMAINS" | while read instance; do
+        DEVICES="$( virsh domblklist $instance | tail -n+3 | grep -v '^$' | awk '{print $1}')"
+	echo "$DEVICES" | while read device; do
+	    echo virsh blkdeviotune $instance $device --read-iops-sec $(config-get read_iops_sec) --write-iops-sec $(config-get write_iops_sec)
+	done
+    done
+}
+
+@hook 'start' 
+do_start() {
+    do_blkdeviotuning
+}
+
+@hook 'update'
+do_update() {
+    do_blkdeviotuning
+}
 
 reactive_handler_main
